6dc3f67163a88d481115e6e900152c771601e7e7
Alan Huerta Magdaleno <aahuerta1@gmail.com>
2021-06-20 17:48:41 -0500

    Managed to create the todo list. The list can add items, delete items, and the user can interact with the tasks. However I did not understand how to use material UI and how to get the date of the items creation to show up.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import Header from "./component/header"
-import './App.css';
+import React, { useState } from 'react';
+//data
+import data from "./data.json";
+
+//components
+import Header from "./component/header";
+import ToDoList from "./ToDoList";
+import ToDoForm from './ToDoForm';
+
+
+//import { TableSortLabel } from '@material-ui/core';
+
 
 function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+
+  //Changes the state of a task to either complete or uncomplete. 
+  //If its complete then the text of the todo list will have a strike through. 
+  //Once changes are made it sets the new To-Do List to the updated version.
+  const handleToggle = (id) => {
+    let mapped  = toDoList.map(task => {
+    return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task };
+    });
+
+    setToDoList(mapped);
+  }
+
+  //Takes the To-Do List and filters through it. 
+  //This then returns all items that are not completed. 
+  //Once we have that new list just set that to be the To-Do List being displayed.
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
   return (
     <div className="App">
     <Header/>
+    
+    <ToDoList 
+      toDoList = { toDoList } 
+      handleToggle = {handleToggle} 
+      handleFilter = {handleFilter} />
+    
+    <ToDoForm 
+      addTask = {addTask} />
     </div>
   );
 }
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,3 +1,16 @@
-export default function Header(){
-    return <div>2021 IBM Accelerate Software Track Lab Boiler Plate</div>
+// export default function Header(){
+//     return <div>2021 IBM Accelerate Software Track Lab Boiler Plate</div>
+// }
+
+
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <header>
+            <h1> To Do List </h1>
+        </header>
+    )
 }
+
+export default Header;
