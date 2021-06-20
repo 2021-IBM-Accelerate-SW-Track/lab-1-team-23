95acccc8b33555ccb01cdc7419f091f395578433
Alan Huerta Magdaleno <aahuerta1@gmail.com>
2021-06-20 17:51:07 -0500

    Testing git commit.

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ToDo from './ToDo';
 
-const ToDoList = ({ toDoList, handleToggle, handleFilter }) => {
-    return ( <
-        div > {
-            toDoList.map(todo => {
-                return ( <
-                    ToDo todo = { todo }
-                    handleToggle = { handleToggle }
-                    handleFilter = { handleFilter }
-                    />
+const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
+    return (
+        <div>
+            {toDoList.map(todo => {
+                return (
+                    <ToDo todo={todo} 
+                    handleToggle={handleToggle} 
+                    handleFilter={handleFilter}/>
                 )
-            })
-        } <
-        button style = {
-            { margin: '20px' } }
-        onClick = { handleFilter } > Delete Completed < /button> <
-        /div>
+            })}
+            <button style={{margin: '20px'}} onClick={handleFilter}>Delete Completed</button>
+        </div>
     );
 };
 
