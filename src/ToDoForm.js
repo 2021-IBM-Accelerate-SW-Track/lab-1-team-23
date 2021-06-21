908e0bb3711b10529b7e4ffd8eed8cb3f6eb829b
Matthias SMartt <smarttmatthias@gmail.com>
2021-06-20 21:56:33 -0400

    Edited the Submit buttons onClick function

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 
 
-const ToDoForm = ({ addTask }) => {
+const ToDoForm = ({ addTask }) => 
+{
 
     const [ userInput, setUserInput ] = useState('');
 
-    const handleChange = (e) => {
+    const handleChange = (e) => 
+    {
         setUserInput(e.currentTarget.value)
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e) => 
+    {
         e.preventDefault();
         addTask(userInput);
         setUserInput("");
@@ -18,9 +21,9 @@ const ToDoForm = ({ addTask }) => {
     return (
         <form onSubmit={handleSubmit}>
             <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
-            <br/>
-            <Button className = 'submit-button'variant="contained" color="primary">Submit</Button>
-        </form>
+            <br />
+            <Button className = 'submit-button' variant="contained" color="primary" style={{margin: '20px'}} onClick={ handleSubmit }> Submit </Button>
+        </form>    
     );
 };
 
