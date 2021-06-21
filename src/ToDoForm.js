942df6024ddfb540649bbe55d1c2697957735ad5
Matthias SMartt <smarttmatthias@gmail.com>
2021-06-21 03:21:27 -0400

    Updated the look of the application with CSS

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,5 +1,7 @@
+
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
+
 
 
 const ToDoForm = ({ addTask }) => 
@@ -20,11 +22,13 @@ const ToDoForm = ({ addTask }) =>
     }
     return (
         <form onSubmit={handleSubmit}>
-            <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+            <input className = 'text-box'value={userInput} type="text" onChange={handleChange} placeholder="Enter a task"/>
             <br />
-            <Button className = 'submit-button' variant="contained" color="primary" style={{margin: '20px'}} onClick={ handleSubmit }> Submit </Button>
+            <Button className = 'submit-button' variant="contained" color="primary" onClick={ handleSubmit }> Add </Button>
         </form>    
     );
 };
+
+
 
 export default ToDoForm;
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -7,7 +7,7 @@ import React from 'react';
 
 const Header = () => {
     return ( 
-        <header>
+        <header className = 'header-content'>
             <h1> To Do List </h1>
         </header>
     )
